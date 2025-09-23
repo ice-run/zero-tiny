398bf30ed6e1db95ccf53747d33fe2c376396592
DaoDao <625117069@qq.com>
2025-09-23 09:37:43 +0800

    临时降级为 java 21

--- zero-server/src/main/java/run/ice/zero/server/ZeroServerApplication.java
+++ zero-server/src/main/java/run/ice/zero/server/ZeroServerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZeroServerApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(ZeroServerApplication.class, args);
     }
 
